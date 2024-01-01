f87cbd11720af4dd1baaf50d6a0ad6d9b80a2f5b
whoisseth <utkarshseth2019@gmail.com>
2024-01-01 22:00:19 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,68 @@
-import Image from 'next/image'
+/** @format */
+
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Balancer from "react-wrap-balancer";
+
+// images
+import databiz from "@/assets/images/client-databiz.svg";
+import audiophile from "@/assets/images/client-audiophile.svg";
+import meet from "@/assets/images/client-meet.svg";
+import maker from "@/assets/images/client-maker.svg";
+
+import hremoImagedesktop from "@/assets/images/image-hero-desktop.png";
+import hremoImageMobile from "@/assets/images/image-hero-mobile.png";
 
 export default function Home() {
+  const bannerImages = [databiz, audiophile, meet, maker];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="min-h-screen w-full bg-gray-100">
+      <Navbar />
+      {/* hero */}
+      <section className="mx-auto flex max-w-6xl  flex-col-reverse gap-2  px-4  pb-12 transition-all md:flex-row md:gap-4">
+        {/* left div */}
+        <div className=" flex flex-col items-center  gap-6 pt-8 text-center md:w-1/2 md:items-start md:gap-10 md:pt-32 md:text-left">
+          <Balancer>
+            <h1 className="text-4xl font-semibold md:text-6xl">
+              Make remote work
+            </h1>
+          </Balancer>
+          <Balancer>
+            <p className=" text-neutral-400 md:max-w-[400px]">
+              Get your team in sync, no matter your location. Streamline
+              processes, create team rituals, and watch productivity soar.
+            </p>
+          </Balancer>
+          <button className="border-balck  w-fit rounded-xl border-2 bg-black px-4 py-2  text-white transition-all hover:border-black hover:bg-black hover:bg-transparent  hover:text-black/90">
+            Learn more
+          </button>
+          <div className="flex gap-2 md:gap-6">
+            {bannerImages.map((img, i) => (
+              <Image
+                className=" h-5 w-auto"
+                key={i}
+                src={img}
+                alt="client-image"
+              />
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        {/* right div */}
+        <section className="md:w-1/2 ">
+          <Image
+            className="hidden h-auto max-w-[400px]  md:block"
+            src={hremoImagedesktop}
+            alt="hreo-image"
+          />
+          <Image
+            className="h-auto w-full  md:hidden"
+            src={hremoImageMobile}
+            alt="hreo-image"
+          />
+        </section>
+      </section>
+    </div>
+  );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,240 @@
+/** @format */
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+import logo from "@/assets/images/logo.svg";
+import Link from "next/link";
+import { IoIosArrowDown } from "react-icons/io";
+
+import todoImage from "@/assets/images/icon-todo.svg";
+import calendarImage from "@/assets/images/icon-calendar.svg";
+import remindersImage from "@/assets/images/icon-reminders.svg";
+import planningImage from "@/assets/images/icon-planning.svg";
+
+import { FiMenu } from "react-icons/fi";
+import { AiOutlineClose } from "react-icons/ai";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
+type NavItem = {
+  label: string;
+  link?: string;
+  children?: NavItem[];
+  iconImage?: string;
+};
+
+const navItems: NavItem[] = [
+  {
+    label: "Features",
+    link: "#",
+    children: [
+      {
+        label: "Todo list",
+        link: "#",
+        iconImage: todoImage
+      },
+      {
+        label: "Calendar",
+        link: "#",
+        iconImage: calendarImage
+      },
+      {
+        label: "Reminders",
+        link: "#",
+        iconImage: remindersImage
+      },
+      {
+        label: "Planning",
+        link: "#",
+        iconImage: planningImage
+      }
+    ]
+  },
+  {
+    label: "Compnay",
+    link: "#",
+    children: [
+      {
+        label: "History",
+        link: "#"
+      },
+      {
+        label: "Our Team",
+        link: "#"
+      },
+      {
+        label: "Blog",
+        link: "#"
+      }
+    ]
+  },
+  {
+    label: "Careers",
+    link: "#"
+  },
+  {
+    label: "About",
+    link: "#"
+  }
+];
+
+export default function Navbar() {
+  const [animationParent] = useAutoAnimate();
+  const [isSideMenuOpen, setSideMenue] = useState(false);
+  function openSideMenu() {
+    setSideMenue(true);
+  }
+  function closeSideMenu() {
+    setSideMenue(false);
+  }
+
+  return (
+    <div className="mx-auto flex  w-full max-w-7xl justify-between px-4 py-5 text-sm">
+      {/* left side  */}
+      <section ref={animationParent} className="flex items-center gap-10">
+        {/* logo */}
+        <Image src={logo} alt=" logo" />
+        {isSideMenuOpen && <MobileNav closeSideMenu={closeSideMenu} />}
+        <div className="hidden md:flex items-center gap-4 transition-all">
+          {navItems.map((d, i) => (
+            <Link
+              key={i}
+              href={d.link ?? "#"}
+              className="relative group  px-2 py-3 transition-all "
+            >
+              <p className="flex cursor-pointer items-center gap-2 text-neutral-400 group-hover:text-black ">
+                <span>{d.label}</span>
+                {d.children && (
+                  <IoIosArrowDown className=" rotate-180  transition-all group-hover:rotate-0" />
+                )}
+              </p>
+
+              {/* dropdown */}
+              {d.children && (
+                <div className="absolute   right-0   top-10 hidden w-auto  flex-col gap-1   rounded-lg bg-white py-3 shadow-md  transition-all group-hover:flex ">
+                  {d.children.map((ch, i) => (
+                    <Link
+                      key={i}
+                      href={ch.link ?? "#"}
+                      className=" flex cursor-pointer items-center  py-1 pl-6 pr-8  text-neutral-400 hover:text-black  "
+                    >
+                      {/* image */}
+                      {ch.iconImage && (
+                        <Image src={ch.iconImage} alt="item-icon" />
+                      )}
+                      {/* item */}
+                      <span className="whitespace-nowrap   pl-3 ">
+                        {ch.label}
+                      </span>
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </Link>
+          ))}
+        </div>
+        {/* navitems */}
+      </section>
+
+      {/* right side data */}
+      <section className=" hidden md:flex   items-center gap-8 ">
+        <button className="h-fit text-neutral-400 transition-all hover:text-black/90">
+          Login
+        </button>
+
+        <button className="h-fit rounded-xl border-2 border-neutral-400 px-4 py-2 text-neutral-400 transition-all hover:border-black hover:text-black/90">
+          Register
+        </button>
+      </section>
+
+      <FiMenu
+        onClick={openSideMenu}
+        className="cursor-pointer text-4xl md:hidden"
+      />
+    </div>
+  );
+}
+
+function MobileNav({ closeSideMenu }: { closeSideMenu: () => void }) {
+  return (
+    <div className="fixed left-0 top-0 flex h-full min-h-screen w-full justify-end bg-black/60 md:hidden">
+      <div className=" h-full w-[65%] bg-white px-4 py-4">
+        <section className="flex justify-end">
+          <AiOutlineClose
+            onClick={closeSideMenu}
+            className="cursor-pointer text-4xl "
+          />
+        </section>
+        <div className=" flex flex-col text-base  gap-2 transition-all">
+          {navItems.map((d, i) => (
+            <SingleNavItem
+              key={i}
+              label={d.label}
+              iconImage={d.iconImage}
+              link={d.link}
+            >
+              {d.children}
+            </SingleNavItem>
+          ))}
+        </div>
+
+        <section className="  flex  flex-col   gap-8  mt-4 items-center">
+          <button className="h-fit text-neutral-400 transition-all hover:text-black/90">
+            Login
+          </button>
+
+          <button className="w-full  max-w-[200px]  rounded-xl border-2 border-neutral-400 px-4 py-2 text-neutral-400 transition-all hover:border-black hover:text-black/90">
+            Register
+          </button>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+function SingleNavItem(d: NavItem) {
+  const [animationParent] = useAutoAnimate();
+  const [isItemOpen, setItem] = useState(false);
+
+  function toggleItem() {
+    return setItem(!isItemOpen);
+  }
+
+  return (
+    <Link
+      ref={animationParent}
+      onClick={toggleItem}
+      href={d.link ?? "#"}
+      className="relative   px-2 py-3 transition-all "
+    >
+      <p className="flex cursor-pointer items-center gap-2 text-neutral-400 group-hover:text-black ">
+        <span>{d.label}</span>
+        {d.children && (
+          // rotate-180
+          <IoIosArrowDown
+            className={`text-xs transition-all  ${isItemOpen && " rotate-180"}`}
+          />
+        )}
+      </p>
+
+      {/* dropdown */}
+      {isItemOpen && d.children && (
+        <div className="  w-auto  flex-col gap-1   rounded-lg bg-white py-3   transition-all flex ">
+          {d.children.map((ch, i) => (
+            <Link
+              key={i}
+              href={ch.link ?? "#"}
+              className=" flex cursor-pointer items-center  py-1 pl-6 pr-8  text-neutral-400 hover:text-black  "
+            >
+              {/* image */}
+              {ch.iconImage && <Image src={ch.iconImage} alt="item-icon" />}
+              {/* item */}
+              <span className="whitespace-nowrap   pl-3 ">{ch.label}</span>
+            </Link>
+          ))}
+        </div>
+      )}
+    </Link>
+  );
+}
